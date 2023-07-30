5fad9a61f4c7e7ae5800f1115cacefbba01b196f
prakashppp <prakashjagtap738760@gmail.com>
2023-07-30 17:23:05 +0530

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormBuilder,Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,30 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'ass14';
+  constructor(public fb:FormBuilder,){
+
+  }
+  m="";
+  // comment="";
+  // post:any;
+  
+  
+  nForm=this.fb.group(
+    {
+      first:['',[Validators.required,Validators.pattern("[a-zA-Z ]*")]],
+      last:['',[Validators.required,Validators.pattern("[a-zA-Z ]*")]],
+      phone:['',[Validators.required,Validators.pattern("^[7-9][0-9]{9}$")]],
+      email:['',[Validators.required,Validators.pattern("^[a-z0-9]+@[a-z]+\.[a-z]{2,}$")]],
+      city:['',[Validators.required,Validators.pattern("^[A-Za-z]{4,}[ ]*$")]],
+      state:['',Validators.required],
+      zipcode:['',[Validators.required,Validators.pattern("^[0-9]{5}")]],
+      comment:['',[Validators.required,Validators.minLength(50)]]
+    }
+    
+  )
+
+  
+
+
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    NgbModule,
+    ReactiveFormsModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
